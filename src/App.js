6984f1996ae4936fedaf5491880048feed19d659
samarth kashyap <samarthkashyap318@gmail.com>
2023-02-24 15:50:43 +0530

    misc:background change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // import Card from "./components/Card";
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 import Bookpage from "./components/Bookpage";
 import About from "./pages/About";
+// import al from "./assets/alchemist.jpg";
 
 function App() {
   return (
-    <div className="bg-gray-700 h-screen text-white">
+    <div
+      className="bg-gray-900 h-full text-white"
+    >
       <Routes>
         <Route
           path="/"
@@ -16,7 +19,7 @@ function App() {
               <Navbar />
               <Main />
             </>
-          }
+          } //src\assets\Hexagon.svg
         />
         <Route path="/about" element={<About />} />
         {/* <Route path="/contact" element={<Contact />} /> */}
